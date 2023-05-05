5e233a5c72c16670d2f9f3be777c7b25a00adb83
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-05-05 20:53:46 +0530

    init with dependencies and splash

---
+++ app/src/main/java/com/nitishsharma/tubeerhai/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nitishsharma.tubeerhai.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nitishsharma.tubeerhai.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
